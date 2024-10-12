7a98bfdb60a6701fa3355f819b3817576d205349
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2024-10-12 17:18:03 +0300

    adding subscriptions feature

---
+++ resources/views/mails/notify_subscriptors.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{$subject}}</title>
+</head>
+<body>
+<h1>Dear Subscriber,</h1>
+<p>{{ $content }}</p>
+<p>Thank you for being with us!</p>
+</body>
+</html>
