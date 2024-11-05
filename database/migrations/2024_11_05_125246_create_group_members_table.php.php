a36ce4f5586cdbc316f2949ad2deadb741639f40
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2024-11-05 17:07:17 +0200

    rename the group members table

--- database/migrations/2024_11_05_125246_create_group_members_table.php.php
+++ database/migrations/2024_11_05_125246_create_group_members_table.php.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_members');
+        Schema::dropIfExists('group_members');
     }
 };
