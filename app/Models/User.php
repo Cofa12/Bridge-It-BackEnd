c71d46c144e9d227350c01ab4622cffe86fee075
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2024-11-06 02:22:34 +0200

    update in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
