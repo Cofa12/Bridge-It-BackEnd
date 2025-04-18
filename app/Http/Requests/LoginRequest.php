63792bb533585aa4e9e2747bab4180bc4b74efef
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2025-04-18 17:39:15 +0200

    fix: the login and the device token process

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -24,6 +24,7 @@ class LoginRequest extends FormRequest
         return [
             'email'=>'required|email|exists:users,email',
             'password'=>'required|string',
+            'device_token'=>'required|string'
         ];
     }
 }
