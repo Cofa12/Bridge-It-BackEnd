5289b38379b889b0a7f19b55edfe532c8d75ecd3
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2025-04-22 17:02:29 +0200

    Refactor:the LandingPageController with form request.

--- app/Http/Requests/AuthRequests/LoginRequest.php
+++ app/Http/Requests/AuthRequests/LoginRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\AuthRequests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ConfirmEmailRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -21,18 +21,10 @@ class ConfirmEmailRequest extends FormRequest
      */
     public function rules(): array
     {
-//        dd($this->input('email'));
         return [
-            'email' => 'required|email|exists:users,email',
-            //
-        ];
-    }
-    public function messages(): array
-    {
-        return [
-            'email.required' => 'Email is required',
-            'email.email' => 'Invalid email format',
-            'email.exists' => 'not found user',
+            'email'=>'required|email|exists:users,email',
+            'password'=>'required|string',
+            'device_token'=>'required|string'
         ];
     }
 }
